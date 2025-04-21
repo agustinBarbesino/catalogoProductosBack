524d4c71e2c16aacc0bc141b67c3009f30bcb50b
Agustín Barbesino <166529387+agustinBarbesino@users.noreply.github.com>
2025-04-21 20:12:24 -0300

    Update signOut.js

--- controllers/Auth/signOut.js
+++ controllers/Auth/signOut.js
@@ -2,10 +2,7 @@ import User from "../../models/User.js"
 
 const signOut = async (req, res) => {
     try {
-        await User.findOneAndUpdate(
-            {email: req.body.email},
-            {inLine: false}
-        )
+        await User.findByIdAndUpdate(req.user._id, { inLine: false })
         return res.status(200).json({
             success: true,
             message: "Signed out successfully"
