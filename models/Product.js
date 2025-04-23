2958413b6d5bccbf998399154feece293adfe927
Agustín Barbesino <166529387+agustinBarbesino@users.noreply.github.com>
2025-04-23 13:51:22 -0300

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,7 @@ let schema = new Schema({
     name: {type:String, required: true},
     description: {type:String, required: true},
     img: {type:String, required: true},
+    price: {type:Number, required: true},
     category: {type:String, required: true},
     catalogType: {type:String, required: true},
 }, {
