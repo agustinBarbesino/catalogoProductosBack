1a9c3276135b8dfcfd2663590d22e0c8aa3a42a6
Agustín Barbesino <166529387+agustinBarbesino@users.noreply.github.com>
2025-04-25 12:05:45 -0300

    Update update.js

--- controllers/Product/update.js
+++ controllers/Product/update.js
@@ -1,16 +1,22 @@
 import Product from "../../models/Product.js";
 
 const updateProduct = async (req, res, next) => {
-    try {
-        const product = await Product.updateOne(
-            { name: req.params.name },
-            { $set: req.body }
-        );
-        return res.status(200).json(product);
-    } catch (error) {
-        next(error);
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id, 
+      { $set: req.body },
+      { new: true } 
+    );
+
+    if (!product) {
+      return res.status(404).json({ message: "Producto no encontrado" });
     }
-}
+
+    return res.status(200).json({ product });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const updateManyProducts = async (req, res, next) => {
     try {
